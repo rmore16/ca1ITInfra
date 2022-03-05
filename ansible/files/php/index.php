0fbb55512760882b1223b223293e1ff5ba4e800b
rmore16 <ruchiramore16@gmail.com>
2022-03-05 17:09:49 +0000

    code changes

--- ansible/files/php/index.php
+++ ansible/files/php/index.php
@@ -6,6 +6,7 @@
 <body>
 <?php
 
+
 // define variables and set to empty values
 $servername = "localhost";
 $username = "bundles_manager";
@@ -18,12 +19,41 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+$servername = "localhost";
+$username = "bundles_manager";
+$password = "Pass1";
+$dbname = "bundles_inventory";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = "INSERT INTO bundles (PLAN_NAME, MINS, DATA)
+        VALUES ('ASIA PLUS', '500', '80 GB');";
+$sql .= "INSERT INTO bundles  (PLAN_NAME, MINS, DATA)
+        VALUES ('EUROPE PLUS', '800', '80 GB');";
+$sql .= "INSERT INTO bundles  (PLAN_NAME, MINS, DATA)
+        VALUES ('UAE PLUS', '900', '60 GB')";
+
+if ($conn->multi_query($sql) === TRUE) {
+          } else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+		  }
+	$conn->close();
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 $sql = "SELECT * FROM bundles";
 $var = mysqli_query($conn, $sql);
 $count = 1;
 
-if (mysqli_num_rows($var) > 0) {
+if (mysqli_num_rows($var) > 4) {
     while ($arr = mysqli_fetch_row($var)) {
         echo "<div class='card bg-dark' style='max-width: 100rem;'>
         <img src='I$count.jpeg' class='card-img'>
@@ -33,7 +63,7 @@ if (mysqli_num_rows($var) > 0) {
           <p class='card-text'><strong>Stay connected with high speed data with limit $arr[2]</strong></p>
         </div>
       </div>";
-     
+
    $count = $count +1; }
     echo "</table>";
     mysqli_free_result($var);
@@ -47,5 +77,6 @@ $conn->close();
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
 </body>
 </html>
